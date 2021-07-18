61c4fda61302a97ea7067bad6a8f691f0d1e307c
rnwelsh <rnwelsh@ualr.edu>
2021-07-18 01:18:23 -0500

    Create main.py
    this should open in vscode, inside the new workspace with virtual environment for python activated if the script worked correctly

---
+++ ve/main.py
@@ -0,0 +1,3 @@
+# If this launched in vscode, you should be ready to go!
+# If you have vscode setup for python, opening the workspace in this folder
+# should activate the virtual environment automatically
