d011a3a7b16d5d20ccaaadaf387731615492c940
rnwelsh <rnwelsh@ualr.edu>
2021-07-18 01:13:15 -0500

    Add files via upload

---
+++ ve/sleep.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+int main(int argc, char* argv[])
+{
+    int n = atoi(argv[1]);
+    Sleep(n);
+    return 0;
+}
